ca93d063e29aa610310a87ed92b493ee13f48382
Yunxiang Zhang <yx.zhang@pku.edu.cn>
2021-03-24 07:21:04 +0000

    1c: init nmt_model

--- nmt_model.py
+++ nmt_model.py
@@ -79,6 +79,14 @@ class NMT(nn.Module):
 
 
 
+        self.encoder = nn.LSTM(embed_size, hidden_size, bias = True, bidirectional = True)
+        self.decoder = nn.LSTMCell(embed_size + hidden_size, hidden_size, bias = True)
+        self.h_projection = nn.Linear(2 * hidden_size, hidden_size, bias = False)
+        self.c_projection = nn.Linear(2 * hidden_size, hidden_size, bias = False)
+        self.att_projection = nn.Linear(2 * hidden_size, hidden_size, bias = False)
+        self.combined_output_projection = nn.Linear(3 * hidden_size, hidden_size, bias = False)
+        self.target_vocab_projection = nn.Linear(hidden_size, len(vocab.tgt), bias = False)
+        self.dropout = nn.Dropout(dropout_rate)
 
         ### END YOUR CODE
 
